dfd888b0303388c686b4f733380b3a789a1e12d3
Faysal Sharif <fsharif@optech4d.com>
2016-10-29 19:44:25 -0500

    Addedrest

---
+++ CodeREDWOrkshop/Assets/Toggle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+
+			if(Physics.Raycast(ray,out hit)) {
+				GameObject selected = hit.collider.gameObject;
+				Debug.Log("Hit");
+				selected.BroadcastMessage ("toggle");
+				selected.GetComponent<MeshRenderer> ().enabled = false;
+			}
+		}
+	}
+}
---
+++ CodeREDWOrkshop/Assets/ToggleCube.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleCube : MonoBehaviour {
+	public GameObject x;
+	public GameObject o;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void toggle () {
+		if (x.GetComponent<MeshRenderer> ().enabled == true) {
+			x.GetComponent<MeshRenderer> ().enabled = false;
+			o.GetComponent<MeshRenderer> ().enabled = true;
+		} 
+		else if(x.GetComponent<MeshRenderer> ().enabled == false && o.GetComponent<MeshRenderer> ().enabled == false) {
+			x.GetComponent<MeshRenderer> ().enabled = false;
+			o.GetComponent<MeshRenderer> ().enabled = true;
+		}
+		else {
+			
+			x.GetComponent<MeshRenderer> ().enabled = true;
+			o.GetComponent<MeshRenderer> ().enabled = false;
+			}
+
+	}
+}
